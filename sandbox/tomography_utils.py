c2ae39815cdf41138f812327f7a3208c04b3d0a7
Uriel Conod <conod6@gmail.com>
2025-02-06 17:16:56 -0800

    Finished the implementation of tomography

--- sandbox/tomography_utils.py
+++ sandbox/tomography_utils.py
@@ -1,14 +1,6 @@
 import numpy as np
 from scipy.sparse import spdiags, coo_matrix, csr_matrix
 
-# Sub2ind utility
-def sub2ind(shape, row, col):
-    return row * shape[1] + col
-
-# ------------------------------------------------------------
-# iCxx: Set bi-harmonic operator (approx to inverse phase covariance matrix)
-# ------------------------------------------------------------
-# We'll create a Laplacian^2 for each layer, store in p_L2, then block diagonal them.
 def make_biharm_operator(n, m):
     """
     Creates a discrete Laplacian operator L^2 using 5-point or 9-point stencils
@@ -48,90 +40,6 @@ def make_biharm_operator(n, m):
 
     return p_L2
 
-def p_bilinearSplineInterp(xo, yo, do, xi, yi):
-    """
-    Sparse bilinear interpolation from a regular grid (xo, yo) with spacing 'do'
-    to query points (xi, yi).
-
-    Parameters
-    ----------
-    xo, yo : 1D arrays
-        The original grid in x and y.  They define a 2D grid of shape (len(yo), len(xo))
-        in row-major style if we do e.g. y for rows, x for columns.
-    do : float
-        The spacing between grid points (assumed uniform).
-    xi, yi : 1D arrays
-        Coordinates of the interpolation points.
-
-    Returns
-    -------
-    P : scipy.sparse.csr_matrix
-        A sparse matrix of shape (len(xi), len(xo)*len(yo)),
-        such that for a field F (flattened as row-major),
-        we can approximate F_i = P * F.
-    """
-    ni = len(xi)
-    nxo = len(xo)
-    nyo = len(yo)
-    no = nxo*nyo
-
-    # Build a mask to remove points out of range
-    mask = (xi >= xo[0]) & (yi >= yo[0]) & (xi <= xo[-1]) & (yi <= yo[-1])
-    valid_idx = np.where(mask)[0]
-
-    # local integer coordinate in the 'xo' and 'yo' sense
-    ox = np.floor((xi[valid_idx] - xo[0]) / do).astype(int)
-    oy = np.floor((yi[valid_idx] - yo[0]) / do).astype(int)
-
-    # local fractional
-    fxo = np.abs(xi[valid_idx] - (xo[0] + do*ox)) / do
-    fyo = np.abs(yi[valid_idx] - (yo[0] + do*oy)) / do
-
-    s1 = (1.0 - fxo)*(1.0 - fyo)
-    s2 = fxo*(1.0 - fyo)
-    s3 = (1.0 - fxo)*fyo
-    s4 = fxo*fyo
-
-    def sub2ind_nxo_nyo(irow, icol, nrow, ncol):
-        return irow + icol*nrow
-
-    o1 = sub2ind_nxo_nyo(oy, ox, nyo, nxo)
-    o2 = sub2ind_nxo_nyo(oy, ox+1, nyo, nxo)
-    o3 = sub2ind_nxo_nyo(oy+1, ox, nyo, nxo)
-    o4 = sub2ind_nxo_nyo(oy+1, ox+1, nyo, nxo)
-
-    # For each of these, some might be out of the domain if ox+1 >= nxo, etc.
-    # We'll mask out the invalid ones:
-    # (Though the initial range-check should handle most.)
-    s1mask = s1 != 0
-    s2mask = s2 != 0
-    s3mask = s3 != 0
-    s4mask = s4 != 0
-
-    row1 = valid_idx[s1mask]
-    col1 = o1[s1mask]
-    data1 = s1[s1mask]
-
-    row2 = valid_idx[s2mask]
-    col2 = o2[s2mask]
-    data2 = s2[s2mask]
-
-    row3 = valid_idx[s3mask]
-    col3 = o3[s3mask]
-    data3 = s3[s3mask]
-
-    row4 = valid_idx[s4mask]
-    col4 = o4[s4mask]
-    data4 = s4[s4mask]
-
-    # Combine
-    rows_all = np.concatenate([row1, row2, row3, row4])
-    cols_all = np.concatenate([col1, col2, col3, col4])
-    data_all = np.concatenate([data1, data2, data3, data4])
-
-    P_coo = coo_matrix((data_all, (rows_all, cols_all)), shape=(ni, no))
-    return P_coo.tocsr()
-
 def cart2pol(x, y):
     """
     Convert Cartesian coordinates (x, y) to polar (theta, rho).
@@ -144,7 +52,7 @@ def cart2pol(x, y):
 
 def sparseGradientMatrixAmplitudeWeighted(validLenslet, amplMask=None, overSampling=2):
     """
-    Computes the sparse gradient matrix (3x3 stencil).
+    Computes the sparse gradient matrix (3x3 stencil) with amplitude mask.
     
     Parameters
     ----------
@@ -241,3 +149,205 @@ def sparseGradientMatrixAmplitudeWeighted(validLenslet, amplMask=None, overSampl
 
     return Gamma, gridMask
 
+def sparseGradientMatrix3x3Stencil(validLenslet):
+    """
+    Computes the sparse gradient matrix (3x3 stencil).
+    
+    Parameters
+    ----------
+    validLenslet : 2D array
+        Valid lenslet map
+
+    Returns
+    -------
+    Gamma : scipy.sparse.csr_matrix
+        Sparse gradient matrix of size.
+    gridMask : 2D array
+        Mask of size (overSampling*validLenslet.shape[0]+1) used for the reconstructed phase.
+    """
+    nLenslet = validLenslet.shape[0] # Number of lenselt across the pupil
+  
+
+    nMap = 2 * nLenslet + 1
+    nValidLenslet_ = np.count_nonzero(validLenslet)
+
+    i0x = np.tile(np.arange(1, 4), 2) # x stencil row subscript
+    j0x = np.repeat(np.arange(1, 4), 2) # x stencil col subscript
+    i0y = np.tile(np.arange(1, 4), 2) # y stencil row subscript
+    j0y = np.repeat(np.arange(1, 4), 2) # y stencil col subscript
+    s0x = np.array([-1/2, -1, -1/2, 1/2, 1, 1/2]) # x stencil weight
+    s0y = -np.array([1/2, -1/2, 1, -1, 1/2, -1/2]) # y stencil weight
+    i_x = np.zeros(6 * nValidLenslet_)
+    j_x = np.zeros(6 * nValidLenslet_)
+    s_x = np.zeros(6 * nValidLenslet_)
+    i_y = np.zeros(6 * nValidLenslet_)
+    j_y = np.zeros(6 * nValidLenslet_)
+    s_y = np.zeros(6 * nValidLenslet_)
+    iMap0, jMap0 = np.meshgrid(np.arange(1, 4), np.arange(1, 4))
+    gridMask = np.zeros((nMap, nMap), dtype=bool)
+    u = np.arange(1, 7)
+
+    # Perform accumulation of x and y stencil row and col subscript and weight
+    for jLenslet in range(1, nLenslet + 1):
+        jOffset = 2 * (jLenslet - 1)
+        for iLenslet in range(1, nLenslet + 1):
+            if validLenslet[iLenslet - 1, jLenslet - 1]:
+                    iOffset = 2 * (iLenslet - 1)
+                    i_x[u - 1] = i0x + iOffset
+                    j_x[u - 1] = j0x + jOffset
+                    s_x[u - 1] = s0x
+                    i_y[u - 1] = i0y + iOffset
+                    j_y[u - 1] = j0y + jOffset
+                    s_y[u - 1] = s0y
+                    u = u + 6
+                    gridMask[iMap0 + iOffset - 1, jMap0 + jOffset - 1] = True
+
+    indx = np.ravel_multi_index((i_x.astype(int) - 1, j_x.astype(int) - 1), (nMap, nMap), order='F')
+    indy = np.ravel_multi_index((i_y.astype(int) - 1, j_y.astype(int) - 1), (nMap, nMap), order='F')
+    v = np.tile(np.arange(1, 2 * nValidLenslet_ + 1), (u.size, 1)).T
+    Gamma = csr_matrix((np.concatenate((s_x, s_y)), (v.flatten() - 1, np.concatenate((indx, indy)))),
+                       shape=(2 * nValidLenslet_, nMap ** 2))
+    Gamma = Gamma[:, gridMask.ravel()]
+
+    return Gamma, gridMask
+
+
+
+def p_bilinearSplineInterp(xo, yo, do, xi, yi):
+    """
+    Sparse bilinear interpolation from a regular grid (xo, yo) with spacing 'do'
+    to query points (xi, yi).
+
+    Parameters
+    ----------
+    xo, yo : 1D arrays
+        The original grid in x and y.  They define a 2D grid of shape (len(yo), len(xo))
+        in row-major style if we do e.g. y for rows, x for columns.
+    do : float
+        The spacing between grid points (assumed uniform).
+    xi, yi : 1D arrays
+        Coordinates of the interpolation points.
+
+    Returns
+    -------
+    P : scipy.sparse.csr_matrix
+        A sparse matrix of shape (len(xi), len(xo)*len(yo)),
+        such that for a field F (flattened as row-major),
+        we can approximate F_i = P * F.
+    """
+    ni = len(xi)
+    nxo = len(xo)
+    nyo = len(yo)
+    no = nxo * nyo
+
+    mask = (xi >= xo[0]) & (yi >= yo[0]) & (xi <= xo[-1]) & (yi <= yo[-1])
+    index = np.flatnonzero(mask)
+    xi_masked = xi[mask]
+    yi_masked = yi[mask]
+
+    ox = np.floor((xi_masked - xo[0]) / do).astype(int) + 1
+    oy = np.floor((yi_masked - yo[0]) / do).astype(int) + 1
+
+    fxo = np.abs(xi_masked - (xo[0] + do * (ox - 1))) / do
+    fyo = np.abs(yi_masked - (yo[0] + do * (oy - 1))) / do
+
+    s1 = (1 - fxo) * (1 - fyo)
+    s2 = fxo * (1 - fyo)
+    s3 = (1 - fxo) * fyo
+    s4 = fxo * fyo
+
+    ox_python = ox - 1
+    oy_python = oy - 1
+
+    o1 = ox_python * nyo + oy_python
+    o2 = (ox_python + 1) * nyo + oy_python
+    o3 = ox_python * nyo + (oy_python + 1)
+    o4 = (ox_python + 1) * nyo + (oy_python + 1)
+
+    masks = [s1 != 0, s2 != 0, s3 != 0, s4 != 0]
+    os = [o1, o2, o3, o4]
+    ss = [s1, s2, s3, s4]
+
+    rows, cols, data = [], [], []
+    for i in range(4):
+        mask_i = masks[i]
+        s = ss[i][mask_i]
+        o = os[i][mask_i]
+        idx = index[mask_i]
+        rows.append(idx)
+        cols.append(o)
+        data.append(s)
+
+    if rows:
+        rows = np.concatenate(rows)
+        cols = np.concatenate(cols)
+        data = np.concatenate(data)
+    else:
+        rows = np.array([], dtype=int)
+        cols = np.array([], dtype=int)
+        data = np.array([], dtype=float)
+
+    P = coo_matrix((data, (rows, cols)), shape=(ni, no))
+    return P.tocsr()
+
+
+import numpy as np
+import math
+
+def create_atm_grid(directionVectorLGS, directionVectorFitSrc, nLayer, dSub, altitude, LGSheight, D):
+    """
+    Create an atmospheric grid for each layer based on the given parameters.
+
+    Parameters:
+    - directionVectorLGS: Direction vector for the LGS (Laser Guide Star).
+    - directionVectorFitSrc: Direction vector for the fitting sources.
+    - nLayer: Number of atmospheric layers.
+    - dSub: Sub-aperture size.
+    - altitude: Array of altitudes for each layer.
+    - LGSheight: Height of the Laser Guide Star.
+    - D: Diameter of the telescope.
+
+    Returns:
+    - atmGrid: List of tuples containing the x and y grids for each layer.
+    """
+    
+    # Extract the x, y components from the direction vectors
+    vg = directionVectorLGS[0:2, :]    # Just the x, y components for LGS
+    vs = directionVectorFitSrc[0:2, :] # x, y components for fitting sources
+
+    atmGrid = []  # Initialize the atmospheric grid list
+    overSampling = np.ones(nLayer) * 2  # Oversampling factor for each layer
+
+    for kLayer in range(nLayer):
+        pitchLayer = dSub / overSampling[kLayer]  # Pitch for the current layer
+        height = altitude[kLayer]  # Altitude of the current layer
+        mVal = 1 - height / LGSheight  # Scaling factor based on height
+
+        # Calculate the displacement vectors for LGS and fitting sources
+        dDirecG = vg * height
+        dDirecS = vs * height
+
+        # Determine the min and max bounds for the grid
+        dmin = np.min(np.concatenate([dDirecG - D/2*mVal, dDirecS - D/2], axis=1), axis=1)
+        dmax = np.max(np.concatenate([dDirecG + D/2*mVal, dDirecS + D/2], axis=1), axis=1)
+
+        # Calculate the number of pixels in the x and y directions
+        nPxLayerX = int(math.floor((dmax[0] - dmin[0]) / pitchLayer)) + 2
+        nPxLayerY = int(math.floor((dmax[1] - dmin[1]) / pitchLayer)) + 2
+
+        # Calculate the total size of the grid in x and y directions
+        Dx = (nPxLayerX - 1) * pitchLayer
+        Dy = (nPxLayerY - 1) * pitchLayer
+
+        # Calculate the starting points for the grid
+        sx = dmin[0] - (Dx - (dmax[0] - dmin[0])) / 2
+        sy = dmin[1] - (Dy - (dmax[1] - dmin[1])) / 2
+
+        # Create the x and y grids
+        x_grid = np.linspace(0, 1, nPxLayerX) * Dx + sx
+        y_grid = np.linspace(0, 1, nPxLayerY) * Dy + sy
+
+        # Append the grid for the current layer to the atmGrid list
+        atmGrid.append((x_grid, y_grid))
+
+    return atmGrid
